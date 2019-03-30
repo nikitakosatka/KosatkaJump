5fca6422fb0861100cd6067e7951633d40cf5db8
nikitakosatka <nikitakosatka@yandex.ru>
2019-03-30 19:50:48 +0500

    Подсчет очков

--- kosatkajump/player.h
+++ kosatkajump/player.h
@@ -11,9 +11,13 @@ class Player {
         HDC player = txLoadImage("data\\images\\ball.bmp");
         int x = 200;
         double y = 900;
-        double speeding = 5;
+        double speeding = 4.5;
         double minY = 350;
         bool moving = false;
+        int score = 0;
+        int highScore = 0;
+        char strScore[1024];
+        char strHighScore[1024];
 
         void update() {
             if (y > minY) {
@@ -28,7 +32,15 @@ class Player {
 
             control();
 
-            txBitBlt(txDC(), x, y, 400, 800, player, 0, 0);
+            if (speeding > 0) {
+                if (score % 1 == 0)
+                    score += (int) speeding;
+                    if (score > highScore)
+                        highScore = score;
+                itoa(score, strScore, 10);
+                itoa(highScore, strHighScore, 10);
+            }
+            txTransparentBlt(txDC(), x, y, 45, 45, player, 0, 0, RGB(255, 255, 255));
         }
 
         void control() {
