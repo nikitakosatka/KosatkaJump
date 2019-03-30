5132e7389453a8b5103b428a5812e909ddb29d01
nikitakosatka <nikitakosatka@yandex.ru>
2019-03-30 19:46:10 +0500

    Исчезающая платформа

--- kosatkajump/game.h
+++ kosatkajump/game.h
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <ctime>
 #include "TXLib.h"
 #include "start_screen.h"
 #include "platform.h"
 #include "player.h"
 #include "death_screen.h"
+#include "disappearing_platform.h"
 
 class Game {
     public:
@@ -27,6 +29,8 @@ class Game {
             Platform platform4(rand() & 200, 700);
             Platform platform5(rand() & 200, -100);
 
+            DisappearingPlatform disPlatform(rand() & 200, -50);
+
             Player player;
 
             txBegin();
@@ -35,11 +39,16 @@ class Game {
                 txBitBlt(txDC(), 0, coordinates[0], 400, 800, background, 0, 0);
                 txBitBlt(txDC(), 0, coordinates[1], 400, 800, background, 0, 0);
 
+                //for (int i = 0; i <= 5; i++) {
+                    //platforms[i].spawn();
+                //}
+
                 platform1.spawn();
                 platform2.spawn();
                 platform3.spawn();
                 platform4.spawn();
                 platform5.spawn();
+                disPlatform.spawn();
 
                 if (coordinates[0] >= 800) {
                     coordinates[0] = 0;
@@ -47,11 +56,17 @@ class Game {
                 }
 
                 else if (player.moving and player.speeding > 0) {
+                    //for (int i = 0; i <= 5; i++) {
+                    //    platforms[i].update();
+                    //}
+
                     platform1.update(player.speeding);
                     platform2.update(player.speeding);
                     platform3.update(player.speeding);
                     platform4.update(player.speeding);
                     platform5.update(player.speeding);
+                    disPlatform.update(player.speeding);
+
                     coordinates[0] += player.speeding / 2;
                     coordinates[1] += player.speeding / 2;
                 }
